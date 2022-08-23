57d763c49b665ef41b5863273f39669776b2ef8b
Quevin Freire <quevinfreire93@gmail.com>
2022-08-23 01:22:45 -0400

    SellerList

--- src/db/DB.java
+++ src/db/DB.java
@@ -9,6 +9,9 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.Properties;
 
+import gui.util.Alerts;
+import javafx.scene.control.Alert.AlertType;
+
 public class DB {
 
 	private static Connection conn = null;
@@ -21,7 +24,8 @@ public class DB {
 			conn = DriverManager.getConnection(url, props);
 			}
 			catch (SQLException e) {
-				throw new DbException (e.getMessage());
+				Alerts.showAlert("Error: DB conections failure", null, e.getMessage(), AlertType.WARNING);
+				//throw new DbException (e.getMessage());
 			}
 		}
 		return conn;
